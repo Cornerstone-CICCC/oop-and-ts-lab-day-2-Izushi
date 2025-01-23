1c7348682a7ea1471f11c550330c5baaab86b46f
Daz <shoeshinesamurai@gmail.com>
2025-01-22 17:37:55 -0800

    oop lab day2

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,16 @@
 import { Component } from "../common/Component.js";
 
 export class AddTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleAdd(todo) {
+    console.log(todo)
+    this.props.todoContext.addTodo(todo)
+  }
+
   render() {
     const addElement = document.createElement('div')
     addElement.className = "add-todo"
@@ -8,6 +18,14 @@ export class AddTodo extends Component {
       <input type="text" id="todo-input" placeholder="Enter task details...">
       <button id="todo-add-btn">Add To Do</button>
     `
+
+    const input = addElement.querySelector('#todo-input')
+    const button = addElement.querySelector('#todo-add-btn')
+
+    button.addEventListener('click', () => {
+      this.handleAdd(input.value)
+      input.value = ''
+    })
 
     return addElement;
   }
